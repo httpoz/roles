67eb193fc0339756436c3fa4bd18942b2f33111a
Oscar Mwanandimai <oscar.mwana@gmail.co.za>
2018-01-02 15:40:12 +0200

    :construction: HasRole trait tests

---
+++ tests/unit/HasRoleTraitTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace HttpOz\Roles\Tests\unit;
+
+use HttpOz\Roles\Models\Role;
+use HttpOz\Roles\Tests\Stubs\User;
+use HttpOz\Roles\Tests\TestCase;
+
+class HasRoleTraitTest extends TestCase
+{
+    public function testCanAttachRole()
+    {
+        $user =  factory(User::class)->create([
+            'name' => 'The Oz'
+        ]);
+        $adminRole = factory(Role::class)->create();
+        $managerRole = factory(Role::class)->create(['name' => 'Manager', 'slug' => 'manager']);
+
+        $user->attachRole($adminRole);
+
+        $this->assertEquals(1, count($user->roles()->count()));
+    }
+}
